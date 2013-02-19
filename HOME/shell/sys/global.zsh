06094ab85ab5d633f0c3224d6b6e5e6223497954
Veniamin Gvozdikov <g.veniamin@googlemail.com>
2013-02-20 00:25:44 +0400

    fixed dir color for autocomplite

--- HOME/shell/sys/global.zsh
+++ HOME/shell/sys/global.zsh
@@ -7,7 +7,7 @@ export GREP_OPTIONS='--color=auto'
 
 #LS
 export LSCOLORS="dxGxFxcxbxDxDxExEx"
-export LS_COLORS='no=00:fi=00:di=00;34:ln=00;36:pi=40;33:so=00;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:ex=00;35:*.cmd=00;32:*.exe=00;32:*.sh=00;32:*.gz=00;31:*.bz2=00;31:*.bz=00;31:*.tz=00;31:*.rpm=00;31:*.cpio=00;31:*.t=93:*.pm=00;36:*.pod=00;96:*.conf=00;33:*.off=00;9:*.jpg=00;94:*.png=00;94:*.xcf=00;94:*.JPG=00;94:*.gif=00;94:*.pdf=00;91'
+export LS_COLORS='no=00:fi=00:di=00;33:ln=00;36:pi=40;33:so=00;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:ex=00;35:*.cmd=00;32:*.exe=00;32:*.sh=00;32:*.gz=00;31:*.bz2=00;31:*.bz=00;31:*.tz=00;31:*.rpm=00;31:*.cpio=00;31:*.t=93:*.pm=00;36:*.pod=00;96:*.conf=00;33:*.off=00;9:*.jpg=00;94:*.png=00;94:*.xcf=00;94:*.JPG=00;94:*.gif=00;94:*.pdf=00;91'
 
 zstyle ':completion:*:default' list-colors "${(s.:.)LS_COLORS}"
 
