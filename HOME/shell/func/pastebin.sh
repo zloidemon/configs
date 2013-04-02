c9e8f49373ee86a9b7521b1ab5fce3bde2b57d5e
Veniamin Gvozdikov <g.veniamin@googlemail.com>
2013-04-02 11:16:39 +0400

    upgraded pastebin script

--- HOME/shell/func/pastebin.sh
+++ HOME/shell/func/pastebin.sh
@@ -3,20 +3,25 @@
 #PRG	"pastebin"
 #INF	"paste your text"
 
-pastebin() {
-	fload(){
-		TFILE=`cat $1|sed 's|%|%25|g;s|&|%26|g;s|+|%2b|g;s|;|%3b|g'`
-		curl -d 'paste_code='$TFILE'' 'http://pastebin.com/api_public.php'
-		echo
+pastebin()
+{
+	fload()
+	{
+		local _tfile 
+		_tfile=${1}
+		TFILE=$(cat ${_tfile}|sed 's|%|%25|g;s|&|%26|g;s|+|%2b|g;s|;|%3b|g')
+		curl -d "api_paste_code=${TFILE}" -d 'api_option=paste' -d \
+			'api_dev_key=b4d8fe0871fb0da5ca94939137cab423' -d 'api_paste_expire_date=1D' \
+			'http://pastebin.com/api/api_post.php'
 	}
 
 	case $1 in
 	
 	-h)
-		echo "Usage:"
-		echo "        pastebin filename for paste a file"
-		echo "    or"
-		echo "        pastebin 'just text' for edit mode"	
+		echo "Usage:" \
+		"        pastebin filename for paste a file" \
+		" or" \
+		"        pastebin 'just text' for edit mode"	
 	;;
 
 	*)	if [ -n "$1" -a -f "$1" ];then	
