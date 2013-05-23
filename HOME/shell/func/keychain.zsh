e5a8c45c010e942be16d0446e24b5464a6386bbd
Veniamin Gvozdikov <g.veniamin@googlemail.com>
2013-05-23 23:41:59 +0400

    [keyload]: added check num args

--- HOME/shell/func/keychain.zsh
+++ HOME/shell/func/keychain.zsh
@@ -1,12 +1,19 @@
 keyload()
 {
+	if [ $# -eq 0 ]
+	then
+		echo "[keyload]: please write keyname"
+		echo "   keyload id_rsa"
+		return 0
+	fi
+
 	local _key
 	_key=${1}
-	echo "start loading ${_key}"
+	echo "[keyload]: start loading ${_key}"
 	if [ ${OSTYPE} != 'Darwin' ] | [  ${USER} != 'root' ]
 	then
 		which keychain 2>/dev/null >/dev/null && eval $(keychain --quiet --eval ${_key})
 	else
-		echo "${_key} couldn't load for ${USER}"
+		echo "[keyload]: ${_key} couldn't load for ${USER}"
 	fi
 }
