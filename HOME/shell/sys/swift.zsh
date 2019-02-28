f6a4b69202347e635c4abf0309de8c2dce483de6
Veniamin Gvozdikov <g.veniamin@googlemail.com>
2019-02-28 17:29:20 +0300

    Removed old FreeBSD kernel configurations and changed support swift on Linux

--- HOME/shell/sys/swift.zsh
+++ HOME/shell/sys/swift.zsh
@@ -1,7 +1,8 @@
 case ${OSTYPE} in
 	Darwin)
-		export TOOLCHAINS=swift
-	;;
+		export TOOLCHAINS=swift;;
+	Linux)
+		export PATH="${PATH}:${HOME}/Devel/Swift/Toolchains/current/usr/bin";;
 esac
 
 alias spi='swift package init'
