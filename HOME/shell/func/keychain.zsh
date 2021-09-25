008a30bcbe4973e4ad7fc202f5a746983e2d3485
Veniamin Gvozdikov <g.veniamin@googlemail.com>
2021-09-26 00:03:07 +0300

    Updated configs

--- HOME/shell/func/keychain.zsh
+++ HOME/shell/func/keychain.zsh
@@ -12,7 +12,7 @@ keyload()
 	echo "[keyload]: start loading ${_key}"
 	if [ ${OSTYPE} != 'Darwin' ] | [ ${USER} != 'root' ]
 	then
-		which keychain 2>/dev/null >/dev/null && eval $(keychain --quiet --eval ${_key})
+		which keychain 2>/dev/null >/dev/null && eval $(keychain --quiet --agents ssh --eval ${_key})
 	else
 		echo "[keyload]: ${_key} couldn't load for ${USER}"
 	fi
